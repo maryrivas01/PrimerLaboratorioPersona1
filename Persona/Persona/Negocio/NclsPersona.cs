35d77c13ea1ec49970cef77a0a1e19ee419a8bb4
Majo06 <am0675032021@unab.edu.sv>
2021-08-17 21:13:51 -0600

    personaMJ

---
+++ Persona/Persona/Negocio/NclsPersona.cs
@@ -0,0 +1,33 @@
+﻿using Persona.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persona.Negocio
+{
+    class NclsPersona
+    {
+        public String Hablar(ClsPersona persona)
+        {
+            persona.Nombre = " Jose ";
+
+            return " La persona " + persona.Nombre + " esta hablando ";
+        }
+        public String  Caminar(ClsPersona persona)
+        {
+            persona.Nombre = " Jose ";
+            return " la persona llamada " + persona.Nombre + " esta caminando ";
+        }
+        public String Reir(ClsPersona persona)
+        {
+            persona.Nombre = "Jose ";
+            return " la persona de nombre " + persona.Nombre + "se esta riendo ";
+        }
+        public String  Observar(ClsPersona persona)
+        {
+            persona.Nombre = " Jose ";
+            return " la persona llamada " + persona.Nombre + " te esta observando ";
+
+        }
+    }
+}
