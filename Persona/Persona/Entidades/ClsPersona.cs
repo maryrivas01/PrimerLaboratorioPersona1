1ceba8dfb30d2160439eaa334816191775d793a8
Mariarivas <87671658+maryrivas01@users.noreply.github.com>
2021-08-17 19:58:00 -0500

    Persona

---
+++ Persona/Persona/Entidades/ClsPersona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persona.Entidades
+{
+    class ClsPersona
+    {
+        private String nombre;
+        private String apellido;
+        private int edad;
+        private String sexo;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+    }
+}
---
+++ Persona/Persona/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Persona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
